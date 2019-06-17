db2c72122c50fb601234e03b47ea1f96c8135627
Matheus <matheusfusco@gmail.com>
2019-06-17 10:13:39 -0300

    Initial Commit

---
+++ GuessTheHeroGame/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GuessTheHeroGame
+//
+//  Created by Matheus Pacheco Fusco on 17/06/19.
+//  Copyright © 2019 Matheus Pacheco Fusco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
